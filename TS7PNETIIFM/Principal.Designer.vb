98a3b7436980ff7d1b8cb938c9d81ac0160ccc8b
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-12-28 14:23:38 -0300

    ADD - MDI container

--- TS7PNETIIFM/Principal.Designer.vb
+++ TS7PNETIIFM/Principal.Designer.vb
@@ -22,19 +22,61 @@ Partial Class Principal
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenúToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AccesoConectadoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AccesoDesconectadoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenúToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(973, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenúToolStripMenuItem
+        '
+        Me.MenúToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AccesoConectadoToolStripMenuItem, Me.AccesoDesconectadoToolStripMenuItem})
+        Me.MenúToolStripMenuItem.Name = "MenúToolStripMenuItem"
+        Me.MenúToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenúToolStripMenuItem.Text = "Menú"
+        '
+        'AccesoConectadoToolStripMenuItem
+        '
+        Me.AccesoConectadoToolStripMenuItem.Name = "AccesoConectadoToolStripMenuItem"
+        Me.AccesoConectadoToolStripMenuItem.Size = New System.Drawing.Size(189, 22)
+        Me.AccesoConectadoToolStripMenuItem.Text = "Acceso Conectado"
+        '
+        'AccesoDesconectadoToolStripMenuItem
+        '
+        Me.AccesoDesconectadoToolStripMenuItem.Name = "AccesoDesconectadoToolStripMenuItem"
+        Me.AccesoDesconectadoToolStripMenuItem.Size = New System.Drawing.Size(189, 22)
+        Me.AccesoDesconectadoToolStripMenuItem.Text = "Acceso desconectado"
         '
         'Principal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(973, 491)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Principal"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Gestión de bonos de empleados - Felipe Mancilla"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenúToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AccesoConectadoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AccesoDesconectadoToolStripMenuItem As ToolStripMenuItem
 End Class
