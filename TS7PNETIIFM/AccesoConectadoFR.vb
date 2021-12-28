b8dac5ef45b4b79ae43d45e725c9e9e0849dce0c
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-12-28 15:03:56 -0300

    ADD - MySQL

--- TS7PNETIIFM/AccesoConectadoFR.vb
+++ TS7PNETIIFM/AccesoConectadoFR.vb
@@ -1,3 +1,18 @@
-﻿Public Class AccesoConectadoFR
+﻿Imports MySql.Data.MySqlClient
+Public Class AccesoConectadoFR
 
+
+
+
+    Private connString As String = "Server=127.0.0.1;User=root;Password=Qwerty123.,;Port=3306;database=Northwind"
+    Dim conn As New MySqlConnection(connString)
+    Dim dataSet As New DataSet()
+    Dim tablaEmpleadores As DataTable
+    Dim query As String
+    Dim numReg As Integer
+
+
+    Private Sub AccesoConectadoFR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
