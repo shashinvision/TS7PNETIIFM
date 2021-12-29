c0f54f9f777aaf3a90b0d301f0c1a16b563829d3
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-12-29 11:45:32 -0300

    ADD - parametros

--- TS7PNETIIFM/AccesoConectadoFR.Designer.vb
+++ TS7PNETIIFM/AccesoConectadoFR.Designer.vb
@@ -32,24 +32,26 @@ Partial Class AccesoConectadoFR
         Me.idOrden = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.nombreCliente = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.pesoCarga = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.fechaOt = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.fechaRequerida = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.fechaEnvio = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.enviadoPor = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.direccionEntrega = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.codigoPostal = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ordenesDeTrabajoGB = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.fechaHasta = New System.Windows.Forms.DateTimePicker()
+        Me.fechaDesde = New System.Windows.Forms.DateTimePicker()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.rangoCargaHastaInput = New System.Windows.Forms.TextBox()
+        Me.rangoCargaDesdeInput = New System.Windows.Forms.TextBox()
+        Me.fechaEnvioDate = New System.Windows.Forms.DateTimePicker()
         Me.RadioButton3 = New System.Windows.Forms.RadioButton()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
-        Me.DateTimePicker3 = New System.Windows.Forms.DateTimePicker()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.empleadosDG, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ordenTrabajoDG, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.ordenesDeTrabajoGB.SuspendLayout()
@@ -102,11 +104,11 @@ Partial Class AccesoConectadoFR
         'ordenTrabajoDG
         '
         Me.ordenTrabajoDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.ordenTrabajoDG.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idOrden, Me.nombreCliente, Me.DataGridViewTextBoxColumn1, Me.fechaOt, Me.fechaRequerida, Me.enviadoPor, Me.direccionEntrega, Me.codigoPostal})
+        Me.ordenTrabajoDG.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idOrden, Me.nombreCliente, Me.DataGridViewTextBoxColumn1, Me.pesoCarga, Me.fechaOt, Me.fechaRequerida, Me.fechaEnvio, Me.enviadoPor, Me.direccionEntrega, Me.codigoPostal})
         Me.ordenTrabajoDG.Location = New System.Drawing.Point(16, 179)
         Me.ordenTrabajoDG.Name = "ordenTrabajoDG"
         Me.ordenTrabajoDG.RowTemplate.Height = 25
-        Me.ordenTrabajoDG.Size = New System.Drawing.Size(919, 168)
+        Me.ordenTrabajoDG.Size = New System.Drawing.Size(944, 168)
         Me.ordenTrabajoDG.TabIndex = 1
         '
         'idOrden
@@ -127,6 +129,11 @@ Partial Class AccesoConectadoFR
         Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
         Me.DataGridViewTextBoxColumn1.ReadOnly = True
         '
+        'pesoCarga
+        '
+        Me.pesoCarga.HeaderText = "Peso Carga"
+        Me.pesoCarga.Name = "pesoCarga"
+        '
         'fechaOt
         '
         Me.fechaOt.HeaderText = "Fecha OT"
@@ -138,6 +145,11 @@ Partial Class AccesoConectadoFR
         Me.fechaRequerida.HeaderText = "Fecha Requerida"
         Me.fechaRequerida.Name = "fechaRequerida"
         Me.fechaRequerida.ReadOnly = True
+        '
+        'fechaEnvio
+        '
+        Me.fechaEnvio.HeaderText = "Fecha Envio"
+        Me.fechaEnvio.Name = "fechaEnvio"
         '
         'enviadoPor
         '
@@ -171,12 +183,12 @@ Partial Class AccesoConectadoFR
         '
         Me.ordenesDeTrabajoGB.Controls.Add(Me.Label4)
         Me.ordenesDeTrabajoGB.Controls.Add(Me.Label3)
-        Me.ordenesDeTrabajoGB.Controls.Add(Me.DateTimePicker3)
-        Me.ordenesDeTrabajoGB.Controls.Add(Me.DateTimePicker2)
+        Me.ordenesDeTrabajoGB.Controls.Add(Me.fechaHasta)
+        Me.ordenesDeTrabajoGB.Controls.Add(Me.fechaDesde)
         Me.ordenesDeTrabajoGB.Controls.Add(Me.Label2)
-        Me.ordenesDeTrabajoGB.Controls.Add(Me.TextBox2)
-        Me.ordenesDeTrabajoGB.Controls.Add(Me.TextBox1)
-        Me.ordenesDeTrabajoGB.Controls.Add(Me.DateTimePicker1)
+        Me.ordenesDeTrabajoGB.Controls.Add(Me.rangoCargaHastaInput)
+        Me.ordenesDeTrabajoGB.Controls.Add(Me.rangoCargaDesdeInput)
+        Me.ordenesDeTrabajoGB.Controls.Add(Me.fechaEnvioDate)
         Me.ordenesDeTrabajoGB.Controls.Add(Me.RadioButton3)
         Me.ordenesDeTrabajoGB.Controls.Add(Me.RadioButton2)
         Me.ordenesDeTrabajoGB.Controls.Add(Me.RadioButton1)
@@ -188,6 +200,79 @@ Partial Class AccesoConectadoFR
         Me.ordenesDeTrabajoGB.TabIndex = 4
         Me.ordenesDeTrabajoGB.TabStop = False
         Me.ordenesDeTrabajoGB.Text = "Órdenes de trabajo"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(827, 79)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 15)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Hasta"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(682, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 15)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Desde"
+        '
+        'fechaHasta
+        '
+        Me.fechaHasta.Enabled = False
+        Me.fechaHasta.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.fechaHasta.Location = New System.Drawing.Point(827, 100)
+        Me.fechaHasta.Name = "fechaHasta"
+        Me.fechaHasta.Size = New System.Drawing.Size(108, 23)
+        Me.fechaHasta.TabIndex = 9
+        '
+        'fechaDesde
+        '
+        Me.fechaDesde.Enabled = False
+        Me.fechaDesde.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.fechaDesde.Location = New System.Drawing.Point(682, 100)
+        Me.fechaDesde.Name = "fechaDesde"
+        Me.fechaDesde.Size = New System.Drawing.Size(108, 23)
+        Me.fechaDesde.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(498, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "-"
+        '
+        'rangoCargaHastaInput
+        '
+        Me.rangoCargaHastaInput.Enabled = False
+        Me.rangoCargaHastaInput.Location = New System.Drawing.Point(516, 97)
+        Me.rangoCargaHastaInput.Name = "rangoCargaHastaInput"
+        Me.rangoCargaHastaInput.Size = New System.Drawing.Size(49, 23)
+        Me.rangoCargaHastaInput.TabIndex = 7
+        '
+        'rangoCargaDesdeInput
+        '
+        Me.rangoCargaDesdeInput.Enabled = False
+        Me.rangoCargaDesdeInput.Location = New System.Drawing.Point(443, 97)
+        Me.rangoCargaDesdeInput.Name = "rangoCargaDesdeInput"
+        Me.rangoCargaDesdeInput.Size = New System.Drawing.Size(49, 23)
+        Me.rangoCargaDesdeInput.TabIndex = 6
+        '
+        'fechaEnvioDate
+        '
+        Me.fechaEnvioDate.Enabled = False
+        Me.fechaEnvioDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.fechaEnvioDate.Location = New System.Drawing.Point(167, 97)
+        Me.fechaEnvioDate.Name = "fechaEnvioDate"
+        Me.fechaEnvioDate.Size = New System.Drawing.Size(108, 23)
+        Me.fechaEnvioDate.TabIndex = 5
         '
         'RadioButton3
         '
@@ -222,77 +307,6 @@ Partial Class AccesoConectadoFR
         Me.RadioButton1.Text = "Fecha de envío"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
-        'DateTimePicker1
-        '
-        Me.DateTimePicker1.Enabled = False
-        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker1.Location = New System.Drawing.Point(167, 97)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(108, 23)
-        Me.DateTimePicker1.TabIndex = 5
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(443, 97)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(49, 23)
-        Me.TextBox1.TabIndex = 6
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(516, 97)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(49, 23)
-        Me.TextBox2.TabIndex = 7
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(498, 100)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(12, 15)
-        Me.Label2.TabIndex = 5
-        Me.Label2.Text = "-"
-        '
-        'DateTimePicker2
-        '
-        Me.DateTimePicker2.Enabled = False
-        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker2.Location = New System.Drawing.Point(682, 97)
-        Me.DateTimePicker2.Name = "DateTimePicker2"
-        Me.DateTimePicker2.Size = New System.Drawing.Size(108, 23)
-        Me.DateTimePicker2.TabIndex = 8
-        '
-        'DateTimePicker3
-        '
-        Me.DateTimePicker3.Enabled = False
-        Me.DateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker3.Location = New System.Drawing.Point(827, 97)
-        Me.DateTimePicker3.Name = "DateTimePicker3"
-        Me.DateTimePicker3.Size = New System.Drawing.Size(108, 23)
-        Me.DateTimePicker3.TabIndex = 9
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(682, 79)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(42, 15)
-        Me.Label3.TabIndex = 10
-        Me.Label3.Text = "Desde"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(827, 79)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(38, 15)
-        Me.Label4.TabIndex = 11
-        Me.Label4.Text = "Hasta"
-        '
         'AccesoConectadoFR
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
@@ -324,20 +338,22 @@ Partial Class AccesoConectadoFR
     Friend WithEvents fechaNacEmpleado As DataGridViewTextBoxColumn
     Friend WithEvents fechaContratoEmpleado As DataGridViewTextBoxColumn
     Friend WithEvents ciudad As DataGridViewTextBoxColumn
+    Friend WithEvents fechaEnvioDate As DateTimePicker
+    Friend WithEvents Label2 As Label
+    Friend WithEvents rangoCargaHastaInput As TextBox
+    Friend WithEvents rangoCargaDesdeInput As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents fechaHasta As DateTimePicker
+    Friend WithEvents fechaDesde As DateTimePicker
     Friend WithEvents idOrden As DataGridViewTextBoxColumn
     Friend WithEvents nombreCliente As DataGridViewTextBoxColumn
     Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents pesoCarga As DataGridViewTextBoxColumn
     Friend WithEvents fechaOt As DataGridViewTextBoxColumn
     Friend WithEvents fechaRequerida As DataGridViewTextBoxColumn
+    Friend WithEvents fechaEnvio As DataGridViewTextBoxColumn
     Friend WithEvents enviadoPor As DataGridViewTextBoxColumn
     Friend WithEvents direccionEntrega As DataGridViewTextBoxColumn
     Friend WithEvents codigoPostal As DataGridViewTextBoxColumn
-    Friend WithEvents DateTimePicker1 As DateTimePicker
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents DateTimePicker3 As DateTimePicker
-    Friend WithEvents DateTimePicker2 As DateTimePicker
 End Class
--- TS7PNETIIFM/AccesoConectadoFR.vb
+++ TS7PNETIIFM/AccesoConectadoFR.vb
@@ -1,11 +1,12 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class AccesoConectadoFR
 
-    Private connString As String = "Server=127.0.0.1;User=root;Password=Qwerty123.,;Port=3306;database=Northwind"
+    Private connString As String = "Server=127.0.0.1;User=root;Password=Qwerty123.,;Port=3306;database=Northwind;Convert Zero Datetime=True"
     Dim conn As New MySqlConnection(connString)
     Dim query As String
     Public comando As MySqlCommand = New MySqlCommand()
     Dim reader As MySqlDataReader
+    Dim idEmpleadoGlobal As Integer
 
 
     Private Sub AccesoConectadoFR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -52,49 +53,103 @@ Public Class AccesoConectadoFR
     End Sub
 
     Private Sub empleadosDG_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles empleadosDG.CellClick
-        Dim idEmpleado = empleadosDG.Rows(e.RowIndex).Cells(0).Value
-        ordenesDeTrabajoGet(idEmpleado)
+        idEmpleadoGlobal = empleadosDG.Rows(e.RowIndex).Cells(0).Value
+        ordenesDeTrabajoGet(idEmpleadoGlobal, "", "", "", "", "")
     End Sub
 
-    Public Sub ordenesDeTrabajoGet(ByVal idEmpleado As Integer)
+    Public Sub ordenesDeTrabajoGet(ByVal Optional idEmpleado As Integer = 0, ByVal Optional fechaEnvioInput As String = "", ByVal Optional pesoCargaDesde As String = "", ByVal Optional pesoCargaHasta As String = "", ByVal Optional rangoFechaOrdenDesde As String = "", ByVal Optional rangoFechaOrdenHasta As String = "")
 
         ordenTrabajoDG.Rows.Clear() ' limpio la tabla para la siguiente busqueda, si no se añadiran registros cada vez que haga una petición
 
-        query = "select o.OrderID as id, c.CompanyName as nombreCliente, concat(e.FirstName, ' ', e.LastName) as nombreEmpleado , o.OrderDate as fechaOT, o.RequiredDate as fechaRequerida, s.CompanyName as enviadoPor, concat(o.ShipAddress, ', ', o.ShipCity, ', ', o.ShipCountry) as direccionEntrega, o.ShipPostalCode as codigoPostal
-                    from orders o 
-                    inner join customers c 
-                    on o.CustomerID = c.CustomerID 
-                    inner join shippers s 
-                    on s.ShipperID = o.ShipVia 
-                    inner join employees e 
-                    on e.EmployeeID = o.EmployeeID 
-                    where o.EmployeeID = @idEmpleado"
-
         Try
-            comando.CommandType = CommandType.Text
-            comando.Connection = conn
-            comando.CommandText = query
-            comando.Parameters.Clear()
-            comando.Parameters.AddWithValue("@idEmpleado", idEmpleado)
+
+            If idEmpleado > 0 Then
+                query = "select
+                            o.OrderID as id, 
+                            c.CompanyName as nombreCliente,
+                            concat(e.FirstName, ' ', e.LastName) as nombreEmpleado , 
+                            o.Freight as carga,
+                            o.OrderDate as fechaOT, 
+                            o.RequiredDate as fechaRequerida, 
+                            o.ShippedDate as fechaEnvio,
+                            s.CompanyName as enviadoPor, 
+                            concat(o.ShipAddress, ', ',
+                            o.ShipCity, ', ', 
+                            o.ShipCountry) as direccionEntrega,
+                            o.ShipPostalCode as codigoPostal
+                            from orders o 
+                            inner join customers c 
+                            on o.CustomerID = c.CustomerID 
+                            inner join shippers s 
+                            on s.ShipperID = o.ShipVia 
+                            inner join employees e 
+                            on e.EmployeeID = o.EmployeeID 
+                            where o.EmployeeID = @idEmpleado;"
+
+                comando.CommandType = CommandType.Text
+                comando.Connection = conn
+                comando.CommandText = query
+                comando.Parameters.Clear()
+                comando.Parameters.AddWithValue("@idEmpleado", idEmpleado)
+
+            ElseIf fechaEnvioinput IsNot "" Then
+
+                query = "select
+                            o.OrderID as id, 
+                            c.CompanyName as nombreCliente,
+                            concat(e.FirstName, ' ', e.LastName) as nombreEmpleado , 
+                            o.Freight as carga,
+                            o.OrderDate as fechaOT, 
+                            o.RequiredDate as fechaRequerida, 
+                            o.ShippedDate as fechaEnvio,
+                            s.CompanyName as enviadoPor, 
+                            concat(o.ShipAddress, ', ',
+                            o.ShipCity, ', ', 
+                            o.ShipCountry) as direccionEntrega,
+                            o.ShipPostalCode as codigoPostal
+                            from orders o 
+                            inner join customers c 
+                            on o.CustomerID = c.CustomerID 
+                            inner join shippers s 
+                            on s.ShipperID = o.ShipVia 
+                            inner join employees e 
+                            on e.EmployeeID = o.EmployeeID 
+                            where o.EmployeeID = @idEmpleado
+                            and substring(date_format(o.ShippedDate, '%d-%m-%Y'), 1, 10) = @fechaEnvio;"
+
+                comando.CommandType = CommandType.Text
+                comando.Connection = conn
+                comando.CommandText = query
+                comando.Parameters.Clear()
+                comando.Parameters.AddWithValue("@idEmpleado", idEmpleadoGlobal)
+                comando.Parameters.AddWithValue("@fechaEnvio", fechaEnvioInput)
+
+
+            End If
+
             conn.Open()
             reader = comando.ExecuteReader()
 
             While reader.Read()
+
                 Dim idOT As Integer = reader(0).ToString
                 Dim nombreCliente As String = reader(1).ToString
                 Dim nombreEmpleado As String = reader(2).ToString
-                Dim fechaOT As String = reader(3).ToString.Substring(0, reader(3).ToString.LastIndexOf(" "))
-                Dim fechaRequerida As String = reader(4).ToString.Substring(0, reader(4).ToString.LastIndexOf(" "))
-                Dim delivery As String = reader(5).ToString
-                Dim direccionEntrega As String = reader(6).ToString
-                Dim codigoPostal As String = reader(7).ToString
+                Dim pesoCarga As String = reader(3).ToString
+                Dim fechaOT As String = reader(4).ToString.Substring(0, reader(4).ToString.LastIndexOf(" "))
+                Dim fechaRequerida As String = reader(5).ToString.Substring(0, reader(5).ToString.LastIndexOf(" "))
+                Dim fechaEnvio As String = reader(6).ToString.Substring(0, reader(6).ToString.LastIndexOf(" "))
+                Dim delivery As String = reader(7).ToString
+                Dim direccionEntrega As String = reader(8).ToString
+                Dim codigoPostal As String = reader(9).ToString
 
-                ordenTrabajoDG.Rows.Add(idOT, nombreCliente, nombreEmpleado, fechaOT, fechaRequerida, delivery, direccionEntrega, codigoPostal)
+                ordenTrabajoDG.Rows.Add(idOT, nombreCliente, nombreEmpleado, pesoCarga, fechaOT, fechaRequerida, fechaEnvio, delivery, direccionEntrega, codigoPostal)
                 ordenTrabajoDG.Refresh()
 
             End While
 
-
+        Catch ex As MySqlException
+            MessageBox.Show("Error Mysql: " & vbCrLf & ex.Message)
 
         Catch ex As Exception
 
@@ -109,5 +164,38 @@ Public Class AccesoConectadoFR
 
 
     End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        fechaEnvioDate.Enabled = True
+        rangoCargaDesdeInput.Enabled = False
+        rangoCargaHastaInput.Enabled = False
+        fechaDesde.Enabled = False
+        fechaHasta.Enabled = False
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        fechaEnvioDate.Enabled = False
+        rangoCargaDesdeInput.Enabled = True
+        rangoCargaHastaInput.Enabled = True
+        fechaDesde.Enabled = False
+        fechaHasta.Enabled = False
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        fechaEnvioDate.Enabled = False
+        rangoCargaDesdeInput.Enabled = True
+        rangoCargaHastaInput.Enabled = True
+        fechaDesde.Enabled = True
+        fechaHasta.Enabled = True
+    End Sub
+
+    Private Sub fechaEnvioDate_ValueChanged(sender As Object, e As EventArgs) Handles fechaEnvioDate.ValueChanged
+
+        Dim fechaEnvio As String = fechaEnvioDate.Value.ToString.Substring(0, fechaEnvioDate.Value.ToString.LastIndexOf(" "))
+
+        ordenesDeTrabajoGet(0, fechaEnvio, "", "", "", "")
+
+    End Sub
+
 
 End Class
